ffdacf3243c3d0bc372c2a3775ba8075fca550df
300pingEnjoyer <va5gromov@yandex.ru>
2022-12-01 11:58:09 +0700

    task 001 complete

---
+++ 001/Program.cs
@@ -0,0 +1,29 @@
+﻿void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+void Show2DArray (double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[rows, columns];
+FillArrayRandomNumbers(numbers);
+Show2DArray(numbers);
